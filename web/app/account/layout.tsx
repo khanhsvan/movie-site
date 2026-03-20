70494e1778bce1cca01a072192b1f1412bde02ca
khanhsvan <118794637+khanhsvan@users.noreply.github.com>
2026-03-21 01:15:28 +0700

    Delete web directory

--- web/app/account/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from 'react';
-import { requireUser } from '../../lib/session';
-
-export default async function AccountLayout({ children }: { children: ReactNode }) {
-  await requireUser();
-  return children;
-}
